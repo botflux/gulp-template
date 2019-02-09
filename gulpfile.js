990e2d39ad41bd681ca1db3769951b7a6c6808d4
botflux <victor.mendele68@gmail.com>
2019-02-09 22:08:33 +0100

    feat: source map

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,31 @@ const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 const concat = require('gulp-concat')
 const clean = require('gulp-clean')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
 
-task('clean', () => {
-    return src('./dist', { read: false, allowEmpty: true })
+task(
+    'clean', 
+    () => src('./dist', { read: false, allowEmpty: true })
         .pipe(clean())
-})
+)
 
-task('minify-js', () => {
-    return src('./src/**/*.js')
+task(
+    'minify-js', 
+    () => src('./src/**/*.js')
+        .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['@babel/env']
         }))
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(dest('./dist/'))
-})
+)
 
-task('js', () => {
-    return series([
-        'clean',
-        'minify-js'
-    ])
-})
+task(
+    'minify-css', 
+    () => src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(dest('./dist/'))
+)
